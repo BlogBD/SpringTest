dccf8d4cd82d9843548778e5d052922482dd7a97
BlogBD <zengweiwymail@163.com>
2019-08-06 09:54:15 +0800

    update

--- SpringIOC/src/main/java/com/spring/demo/test/Demo01.java
+++ SpringIOC/src/main/java/com/spring/demo/test/Demo01.java
@@ -3,6 +3,8 @@ package com.spring.demo.test;
 import com.spring.demo.service.Impl.UserServiceImpl;
 import com.spring.demo.service.UserService;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Demo01 {
     /**
@@ -11,6 +13,18 @@ public class Demo01 {
     @Test
     public void test01(){
         UserService userService = new UserServiceImpl();
+        userService.save();
+    }
+
+    /**
+     * spring方式的调用
+     */
+    @Test
+    public void test02(){
+        //创建spring工厂
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //通过工厂获取类
+        UserService userService = (UserService)applicationContext.getBean("UserService");
         userService.save();
     }
 }
