521a234b3e7d0ceb60dabfa3e00dc0e47a75088f
BlogBD <zengweiwymail@163.com>
2019-08-08 10:54:11 +0800

    update

---
+++ SpringIOC/src/main/java/com/spring/demo/Demo4/CollectionBean.java
@@ -0,0 +1,38 @@
+package com.spring.demo.Demo4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 集合属性的注入
+ */
+public class CollectionBean {
+    private String [] arrs;
+    private Map map;
+    private List<Person> list;
+    public void setArrs(String[] arrs) {
+        this.arrs = arrs;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setList(List<Person> list) {
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "CollectionBean{" +
+                "arrs=" + Arrays.toString(arrs) +
+                ", map=" + map +
+                ", list=" + list +
+                '}';
+    }
+}
---
+++ SpringIOC/src/main/java/com/spring/demo/test/Demo04.java
@@ -0,0 +1,27 @@
+package com.spring.demo.test;
+
+import com.spring.demo.Demo4.CollectionBean;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/**
+ * 集合类型的属性注入
+ */
+public class Demo04 {
+    /**
+     * 注入数组测试
+     */
+    @Test
+    public void test01(){
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        CollectionBean collectionBean = (CollectionBean) applicationContext.getBean("collectionBean");
+        System.out.println(collectionBean.toString());
+
+    }
+    @Test
+    public void test02(){
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        CollectionBean collectionBean = (CollectionBean) applicationContext.getBean("collectionBean");
+        System.out.println(collectionBean.toString());
+
+    }
+}
