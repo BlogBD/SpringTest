d267911586432408968394eefb23affa18e5c896
BlogBD <zengweiwymail@163.com>
2019-08-08 22:01:39 +0800

    update

--- SpringAOP/src/main/java/com/spring/demo/Calculator.java
+++ SpringAOP/src/main/java/com/spring/demo/Calculator.java
@@ -1,6 +1,8 @@
 package com.spring.demo;
 
+import org.springframework.stereotype.Component;
 
+@Component(value = "calculator")
 public class Calculator {
     public int add(int  a,int b){
         return a+b;
--- SpringAOP/src/main/java/com/spring/demo/LoggingAspect.java
+++ SpringAOP/src/main/java/com/spring/demo/LoggingAspect.java
@@ -5,13 +5,14 @@ package com.spring.demo;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
-
+@Component(value = "loggingAspect")
 @Aspect
 public class LoggingAspect {
-    @Before("execution(* com.spring.demo.Calculator.*(..))")
-    private void arithmeticDoLog(JoinPoint joinPoint){
-        System.out.println(joinPoint.toString());
+    @Before("execution(* com.spring.demo.Calculator.*(..)) && args(a,b,..)")
+    private void arithmeticDoLog(JoinPoint joinPoint ,int a ,int b){
+        System.out.println( a+" and "+ b+": "+joinPoint.toString());
     }
 }
 
