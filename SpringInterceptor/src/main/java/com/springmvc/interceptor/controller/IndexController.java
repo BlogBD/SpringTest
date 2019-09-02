fa07cf65618b1951ea1761ac74ec9d7703ed096f
BlogBD <zengweiwymail@163.com>
2019-09-02 22:57:19 +0800

    update

--- SpringInterceptor/src/main/java/com/springmvc/interceptor/controller/IndexController.java
+++ SpringInterceptor/src/main/java/com/springmvc/interceptor/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.springmvc.interceptor.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 通过spring mvc 减少servlet
+ */
 @Controller
 public class IndexController {
     /**
